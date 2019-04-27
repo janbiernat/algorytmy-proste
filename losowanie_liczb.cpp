3fc05ee65ac63b0d4aa9c6b10a5d328542d1cd13
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2019-04-27 23:13:26 +0200

    Update losowanie_liczb.cpp

--- losowanie_liczb.cpp
+++ losowanie_liczb.cpp
@@ -8,53 +8,53 @@
 #include <cstdlib> /*Posiada instrukcje do zainicjowania 
                      generatora liczb pseudolosowych.*/ 
 #include <ctime> /*Biblioteka "ctime " zawiera funkcje 
-                   zwi¹zane z obs³ug¹ czasu i daty.*/ 
+                   zwiÄ…zane z obsÅ‚ugÄ… czasu i daty.*/ 
 using namespace std; 
 int main() { 
-  cout << "--== Losowanie liczb ==--"; /*Wyœwietlenie na ekranie 
-                                         tekstu znajduj¹cego 
-                                         siê pomiêdzy cudzys³owami.*/ 
-  const int Zakres = 34; /*Zadeklarowanie sta³ej liczbowej 
-                           ca³kowitej "Zakres" i przypisanie 
-                           jej konkretnej wartoœci (np. 34).*/ 
+  cout << "--== Losowanie liczb ==--"; /*WyÅ›wietlenie na ekranie 
+                                         tekstu znajdujÄ…cego 
+                                         siÄ™ pomiÄ™dzy cudzysÅ‚owami.*/ 
+  const int Zakres = 34; /*Zadeklarowanie staÅ‚ej liczbowej 
+                           caÅ‚kowitej "Zakres" i przypisanie 
+                           jej konkretnej wartoÅ›ci (np. 34).*/ 
   srand(time(NULL)); //Zainicjowanie generatora liczb pseudolosowych. 
-  int Liczba=0; /*Zadeklarowanie zmiennej liczbowej ca³kowitej 
-                  "Liczba" z równoczesnym wyzerowaniem zmiennej.*/ 
+  int Liczba=0; /*Zadeklarowanie zmiennej liczbowej caÅ‚kowitej 
+                  "Liczba" z rÃ³wnoczesnym wyzerowaniem zmiennej.*/ 
   Liczba = rand()%Zakres; /*Wylosowanie jednej liczby z podanego zakresu, 
-                            który przechowywany jest w sta³ej liczbowej 
-                            ca³kowitej "Zakres" i przypisanie 
+                            ktÃ³ry przechowywany jest w staÅ‚ej liczbowej 
+                            caÅ‚kowitej "Zakres" i przypisanie 
                             wylosowanej liczby do zmiennej liczbowej 
-                            ca³kowitej "Liczba". 
-                            Funkcja RAND - umo¿liwia wylosowanie jednej 
+                            caÅ‚kowitej "Liczba". 
+                            Funkcja RAND - umoÅ¼liwia wylosowanie jednej 
                                            liczby z podanego zakresu.*/ 
-  cout << "\n\nWylosowano liczbe " << Liczba; /*Obni¿enie kursora 
+  cout << "\n\nWylosowano liczbe " << Liczba; /*ObniÅ¼enie kursora 
                                                 tekstowego o dwa 
-                                                wiersze i wyœwietlenie 
+                                                wiersze i wyÅ›wietlenie 
                                                 na ekranie tekstu 
-                                                znajduj¹cego siê 
-                                                pomiêdzy cudzys³owami 
-                                                oraz wyœwietlenie 
-                                                na ekranie zawartoœci 
+                                                znajdujÄ…cego siÄ™ 
+                                                pomiÄ™dzy cudzysÅ‚owami 
+                                                oraz wyÅ›wietlenie 
+                                                na ekranie zawartoÅ›ci 
                                                 zmiennej liczbowej 
-                                                ca³kowitej "Liczba". 
-                                                Znak "\n" umo¿liwia 
+                                                caÅ‚kowitej "Liczba". 
+                                                Znak "\n" umoÅ¼liwia 
                                                 przeniesienie tekstu 
                                                 do kolejnego wiersza. 
                                                 Parametry oddzielane 
-                                                s¹ znakiem "<<".*/ 
+                                                sÄ… znakiem "<<".*/ 
   cout << "\nWylosowana liczba jest liczba "; 
-  if(Liczba % 2 == 0) {  cout << "parzysta."; } 
+  if(Liczba % 2 == 0) { cout << "parzysta."; } 
   else { cout << "nieparzysta."; } 
-  /*"if(Liczba % 2 == 0)" - Je¿eli warunek jest spe³niony 
-                            (tj. zawartoœæ zmiennej liczbowej 
-                            ca³kowitej "Liczba" podzielonej 
+  /*"if(Liczba % 2 == 0)" - JeÅ¼eli warunek jest speÅ‚niony 
+                            (tj. zawartoÅ›Ä‡ zmiennej liczbowej 
+                            caÅ‚kowitej "Liczba" podzielonej 
                             przez dwa posiada po przecinku zero), 
-                            to wyœwietl informacjê "parzysta". 
-                            W innym przypadku wyœwietl 
-                            informacjê "nieparzysta". 
-                            Znak "%" - Umo¿liwia sprawdzenie liczby 
+                            to wyÅ›wietl informacjÄ™ "parzysta". 
+                            W innym przypadku wyÅ›wietl 
+                            informacjÄ™ "nieparzysta". 
+                            Znak "%" - UmoÅ¼liwia sprawdzenie liczby 
                                        po przecinku.*/ 
-  //Naciœnij dowolny klawisz. 
+  //NaciÅ›nij dowolny klawisz. 
   cout << "\n\nNacisnij dowolny klawisz..."; 
   getch(); 
 } 
