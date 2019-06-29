aeef3049f795c12ad64d7bc85b859597ea6084f5
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2019-06-29 14:57:00 +0200

    Update tabliczka-mnozenia-w2.cpp

--- tabliczka-mnozenia-w2.cpp
+++ tabliczka-mnozenia-w2.cpp
@@ -1,23 +1,24 @@
+#include <iostream> /*Biblioteka "iostream" (ang. input/output stream) 
+                      oznacza strumieÅ„ wejÅ›cia/wyjÅ›cia. 
+                      Za pomocÄ… tej biblioteki moÅ¼na m.in. wprowadzaÄ‡ 
+                      informacje ze standardowych urzÄ…dzeÅ„ 
+                      wejÅ›cia (klawiatura) lub wyprowadzaÄ‡ 
+                      informacje ze standardowych urzÄ…dzeÅ„ 
+                      wyjÅ›cia (ekran).*/ 
+#include <conio.h>  /*ObsÅ‚uga klawiatury za pomocÄ….*/ 
+#include <cstdlib>  /*Posiada m.in. instrukcje do zainicjowania generatora liczb pseudolosowych.*/ 
+#include <ctime>    /*Biblioteka "ctime " zawiera funkcje zwiÄ…zane z obsÅ‚ugÄ… czasu i daty.*/ 
+#include <string.h> /*PodÅ‚Ä…czenie biblioteki umoÅ¼liwiajÄ…cej obsÅ‚ugÄ™ Å‚aÅ„cuchÃ³w znakÃ³w.*/ 
 /*--== Borland C++Builder (Console Application) ==-- 
                                 Kurs podstawowy 
  Copyright(c)by Jan T. Biernat*/ 
-#include <iostream> /*Biblioteka "iostream" (ang. input/output stream) 
-                      oznacza strumieñ wejœcia/wyjœcia. 
-                      Za pomoc¹ tej biblioteki mo¿na m.in. wprowadzaæ 
-                      informacje ze standardowych urz¹dzeñ 
-                      wejœcia (klawiatura) lub wyprowadzaæ 
-                      informacje ze standardowych urz¹dzeñ 
-                      wyjœcia (ekran).*/ 
-#include <conio.h>  /*Obs³uga klawiatury za pomoc¹.*/ 
-#include <cstdlib>  /*Posiada m.in. instrukcje do zainicjowania generatora liczb pseudolosowych.*/ 
-#include <ctime>    /*Biblioteka "ctime " zawiera funkcje zwi¹zane z obs³ug¹ czasu i daty.*/ 
-#include <string.h> /*Pod³¹czenie biblioteki umo¿liwiaj¹cej obs³ugê ³añcuchów znaków.*/ 
 using namespace std; 
-//Blok g³ówny. 
+//Blok gÅ‚Ã³wny. 
 int main() { 
   cout << "--== Tabliczka mnozenia w2 ==--\n"; 
   cout << "Copyright (c)by Jan T. Biernat\n"; 
-  //Deklaracja sta³ych. 
+  cout << "UWAGA: Wpisanie wartosci mniejszej od 0, spowoduje wyjscie z programu!\n\n"; 
+  //Deklaracja staÅ‚ych. 
     const int iIloscKomentarzy = 5; 
     const string tSpc0 = "   "; 
     const string tKomentarzePozytywne[iIloscKomentarzy] = {"Bardzo dobrze", 
@@ -40,7 +41,7 @@ int main() {
     if(iPoziom < 5) iPoziom = 5; 
     else if(iPoziom > 31) iPoziom = 31; 
     cout << "\nBede losowal liczby z zakresu od 0 do " << iPoziom << ".\n"; 
-  //Losowanie pytañ. 
+  //Losowanie pytaÅ„. 
     do{ 
       iLicznik++; //Licznik = Licznik+1; 
       srand(time(NULL)); //Zainicjowanie generatora liczb pseudolosowych. 
@@ -51,37 +52,37 @@ int main() {
           B = 0; B = rand()%(iPoziom+1); 
       } 
       cout << "\n\n" << iLicznik << ") " << A << " x " << B << " = "; 
-      //Sprawdzenie, czy odpowiedŸ u¿ytkownika jest prawid³owa. 
+      //Sprawdzenie, czy odpowiedÅº uÅ¼ytkownika jest prawidÅ‚owa. 
         iOdp = 0; cin >> iOdp; 
-        if(iOdp != -1) { 
+        if(iOdp > -1) { 
           string tKomentarz = "", tPrawidlowyWynik = ""; 
           if(A*B == iOdp) { 
-            iPozytywne++; //Zliczaj prawid³owe odpowiedzi. 
+            iPozytywne++; //Zliczaj prawidÅ‚owe odpowiedzi. 
             cout << tSpc0 << tKomentarzePozytywne[rand()%(iIloscKomentarzy)] << "."; 
             logJest = true; 
           } else { 
-                    iNegatywne++; //Zliczaj b³êdne odpowiedzi. 
+                    iNegatywne++; //Zliczaj bÅ‚Ä™dne odpowiedzi. 
                     cout << tSpc0 << tKomentarzeNegatywne[rand()%(iIloscKomentarzy)] << "!"; 
                     cout << "\n" << tSpc0 << "Prawidlowa odpowiedz to " << (A*B) << "."; 
                     logJest = false; 
                  } 
         } 
-    } while(iOdp != -1); 
+    } while(iOdp > -1); 
   //Raport z przeprowadzonego testu. 
     if(iLicznik-1 > 0) { 
       cout << "\n\nIlosc zadanych pytan: " << iLicznik-1; 
       iOcena = 0; iOcena = iPozytywne * 100 / (iLicznik-1); 
       cout << "\n  Odpowiedzi poprawnych : " << iPozytywne << " (" << iOcena << "%)"; 
       cout << "\n  Odpowiedzi negatywnych: " << iNegatywne << " (" << iNegatywne * 100 / (iLicznik-1) << "%)"; 
-      //Wyœwietl ocenê. 
+      //WyÅ›wietl ocenÄ™. 
         string tOcena = ""; 
         if(iOcena > 95) tOcena = "Bardzo dobry (5.0)"; 
         else if(iOcena > 80) tOcena = "Dobry (4.0)"; 
              else if(iOcena > 70) tOcena = "Dostateczny (3.0)"; 
                   else if(iOcena > 50) tOcena = "Mierny (2.0)"; 
                        else if(iOcena <= 50) tOcena = "Jedynka (1.0)"; 
-        cout << "\nOtrzymana ocena to: " << tOcena;
-      //Naciœnij dowolny klawisz. 
+        cout << "\nOtrzymana ocena to: " << tOcena; 
+      //NaciÅ›nij dowolny klawisz. 
         cout << "\n\nNacisnij dowolny klawisz..."; getch(); 
     } 
 } 
