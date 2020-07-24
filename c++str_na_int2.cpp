d98090fc2948b7a7ec38ef00a929e321d347dd61
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2020-07-24 05:02:32 +0200

    Add files via upload

---
+++ c++str_na_int2.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+#include <cstdlib> 
+using namespace std; 
+/*--== Konwersja STRING na INT 2 ==-- 
+  Copyright (c)by Jan T. Biernat 
+  = 
+  Napisz program, który wykona konwersję 
+  liczby traktowanej jako tekst na liczbę 
+  typu INT(ang. integer). 
+*/ 
+int StrToInt(string Str = "") { 
+  //StrToInt - Konwersja liczby taktowanej jako tekst na typ INT(ang. integer). 
+  const string Inf = "\nBLAD -?Brak danych!\n"; 
+  string Cyfry = ""; 
+  short int Poz = 0; 
+  if(Str != "") { 
+    if(Str[0] == '-') { Poz = 1; } 
+    for(int I = Poz; I < Str.length(); I++) { 
+      if(((int)Str[I] > 47) && ((int)Str[I] < 58)) { Cyfry += Str[I]; } //"Cyfry += Str[I];" = "Cyfry = Cyfry+Str[I];". 
+    } 
+    if(Poz == 1) { Cyfry = Str[0]+Cyfry; } 
+    if(Cyfry != "") { return atoi(Cyfry.c_str()); } 
+    else { cout << Inf; return 0; } 
+  } else { cout << Inf; return 0; } 
+} 
+//Blok główny (startowy). 
+int main() { 
+  cout << "--== Konwersja STRING na INT 2 ==--\n"; 
+  cout << "Copyright (c)by Jan T. Biernat\n\n"; 
+  string Liczba = ""; 
+  cout << "\nLiczba: "; getline(cin, Liczba); 
+  cout << "Podano \"" << Liczba << "\".\n"; 
+  cout << "\nSuma: \"" << (StrToInt(Liczba) + StrToInt(Liczba)) << "\"."; 
+  cout << "\n"; 
+} 
