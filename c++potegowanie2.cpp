e157e2f8a868564521b768c0302c5635f66a3aec
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2025-10-31 14:05:10 +0100

    Add files via upload

---
+++ c++potegowanie2.cpp
@@ -0,0 +1,43 @@
+#include <iostream> 
+#include <conio.h> 
+/*--== Potęgowanie ==-- 
+ Copyright (c)by Jan T. Biernat 
+ = 
+ Potęgowanie jest działaniem matematycznym 
+ i polega na wielokrotnym mnożeniu liczby przez samą siebie. 
+ Liczba, którą będziemy potęgować nazywa się podstawą. 
+ Natomiast liczbę określającą ile razy pomnożymy 
+ podstawę przez samą siebie, nazywa się wykładnikiem. 
+*/ 
+using namespace std; 
+long int Potega(long int Podstawa, int Wykladnik = 0) { 
+  long int Wynik = 1; //Przypisz do zmiennej "Wynik" domyślną wartość 1, 
+                      //gdy zmienna "Wykladnik" będzie przechowywała wartość 0. 
+  for(int I = 0; I < Wykladnik; I++) { 
+    Wynik *= Podstawa; 
+  } 
+  return Wynik; 
+} 
+long int Potega2(long int Podstawa, int Wykladnik = 0) { 
+  long int Wynik = 1; //Przypisz do zmiennej "Wynik" domyślną wartość 1, 
+                      //gdy zmienna "Wykladnik" będzie przechowywała wartość 0. 
+  for(int I = 0; I < Wykladnik; I++) { 
+    Wynik = Wynik*Podstawa; 
+  } 
+  return Wynik; 
+} 
+//Blok główny/startowy. 
+int main() { 
+  cout << "--== Potegowanie, w2 ==--\n"; 
+  cout << "Copyright (c)by Jan T. Biernat\n\n\n"; 
+  //Deklaracja zmiennych znakowych. 
+    int Podstawa = 0, Wykladnik = 0; 
+  //Pobierz dane z klawiatury. 
+    cout << "Wpisz podstawe : "; cin >> Podstawa; 
+    cout << "Wpisz wykladnik: "; cin >> Wykladnik; 
+  //Wywołanie funkcji. 
+    cout << "\n" << Podstawa << "^" << Wykladnik << " = " << Potega(Podstawa, Wykladnik); 
+    cout << "\n" << Podstawa << "^" << Wykladnik << " = " << Potega2(Podstawa, Wykladnik); 
+  //Czekaj, aż użytkownik naciśnie klawisz ENTER. 
+    cout << "\n\nNacisnij klawisz ENTER..."; getch(); 
+} 
