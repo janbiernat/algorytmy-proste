b440b21d6bde26ec29d73c2cf0c60ebf2b67d21f
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2026-03-06 22:43:52 +0100

    Add files via upload

---
+++ c++ilosc_ocen_na_procenty.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+/*
+--== Iloœæ ocen na procenty ==--
+Zadanie: Napisz program, który obliczy sumê
+         iloœci poszczególnych ocen
+         oraz procentowy udzia³
+         iloœci poszczególnych ocen.
+*/
+double DwieLiczbyPo(double Liczba) {
+  return(int(Liczba*100)/100.0);
+}
+//
+//Blok g³ówny.
+int main() {
+  cout << "--== Ilosc ocen na procenty ==-- \n";
+  cout << "Copyright (c)by Jan T. Biernat \n \n";
+  //
+  //Deklaracja zmiennych.
+    short LiczOceny = 0;
+    int Dane[6];
+    int Suma = 0;
+    float Procent = 0, SumaProc = 0;
+  //
+  //Wpisanie do tablicy wartoœci zerowych.
+    for(int I = 0; I < 6; I++) { Dane[I] = 0; }
+  //
+  //Pobranie danych liczbowych od u¿ytkownika i zapisanie ich do tablicy.
+    cout << "\nPodaj ilosc poszczegolnych ocen: \n";
+    for(int I = 0; I < 6; I++) {
+      cout << (I+1) << " jest: ";
+      cin >> Dane[I];
+      if(Dane[I] < 1) { Dane[I] = 0; }
+      if(Dane[I] > 999) { Dane[I] = 999; }
+      Suma+= Dane[I];
+    }
+  //
+  //Raport.
+    cout << "\n \nRaport: \n";
+    cout << "Suma ocen wynosi: " << Suma << "\n \n";
+    cout << "Procentowy udzial ocen: \n";
+    for(int I = 0; I < 6; I++) {
+      cout << "  " << (I+1) << " jest: ";
+      cout.width(5); cout.fill(' ');
+      cout << Dane[I] << " (";
+      cout.width(3); cout.fill(' ');
+      Procent = 0; Procent = DwieLiczbyPo((Dane[I]*100.0)/Suma);
+      cout << Procent << "%)\n";
+      SumaProc+= Procent; //SumaProc = SumaProc+Procent;
+    }
+    cout << "\nSuma (%): " << SumaProc;
+  //
+  //Naciœnij dowolny klawisz ...
+    cout << "\n \n Nacisnij dowolny klawisz ..."; getch();
+cout << "\n \n";
+}
